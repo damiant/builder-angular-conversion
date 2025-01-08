a7994bfb5cc07b946d39eb070e2bd9e65ef4f4e5
Damian Tarnawsky <damian@builder.io>
2025-01-08 08:47:54 -0800

    feat: call track conversion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { track } from '@builder.io/sdk-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
+
+  ngOnInit(): void {
+    track({
+      type: 'conversion',
+      amount: 500,
+      // Replace this with the Public API Key from your Builder account
+      apiKey: 'd6151a3b3c4f49dda6bb6c6541d060ee'
+    });
+  }
 }
